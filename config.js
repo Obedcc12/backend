9c86b8aac4ba422369140ca1e7c56caae39f33ff
unknown <cortescastroo@gmail.com>
2024-12-04 18:02:43 -0500

    Back1.2

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-require ("dotenv").config()
+require ("dotenv").config();
 
 const dbconnect =()=> {
     mongoose.set("strictQuery", true)
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URI)
     .then((succes) => console.log("Conexion exitosa"))
     .catch((err) => console.log(err.message));
 }
